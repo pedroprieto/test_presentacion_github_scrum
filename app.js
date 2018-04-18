274c61aaa806800d70bc08447014ea709c040faa
Pedro Prieto <pedpral@gmail.com>
2018-04-18 20:24:02 +0200

    Cambio OpenShift por Heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Load the http module to create an http server.
 var http = require('http');
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var host = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.PORT || 8080;
+var host = '127.0.0.1';
 
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
