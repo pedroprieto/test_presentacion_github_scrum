a49d37c5d5b1bab8d7508cacf28426f88656889d
Pedro Prieto <pedpral@gmail.com>
2018-04-18 20:42:37 +0200

    Fix?

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var server = http.createServer(function (request, response) {
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(port, host);
+server.listen(port);
 
 // Put a friendly message on the terminal
 console.log("Server running");
